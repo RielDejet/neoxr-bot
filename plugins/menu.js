1488a2a8f9daa2d355229e01b1a3d11b5ae5c8e1
RielDejet <109830424+RielDejet@users.noreply.github.com>
2022-08-21 12:05:31 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -5,35 +5,35 @@ exports.run = {
       isPrefix
    }) => {
       let rows = [{
-         title: 'DOWNLOADER',
+         title: 'DOWNLOADER📩',
          rowId: `${isPrefix}menutype 1`,
          description: ``
       }, {
-         title: 'GROUP TOOLS',
+         title: 'GROUP TOOLS🗂️',
          rowId: `${isPrefix}menutype 2`,
          description: ``
       }, {
-         title: 'TEXT MAKER',
+         title: 'TEXT MAKER📭',
          rowId: `${isPrefix}menutype 3`,
          description: ``
       }, {
-         title: 'USER INFO',
+         title: 'USER INFO💌',
          rowId: `${isPrefix}menutype 4`,
          description: ``
       }, {
-         title: 'UTILITIES',
+         title: 'UTILITIES☢️',
          rowId: `${isPrefix}menutype 5`,
          description: ``
       }, {
-         title: 'OWNER TOOLS',
+         title: 'OWNER TOOLS🔰',
          rowId: `${isPrefix}menutype 6`,
          description: ``
       }, {
-         title: 'SPECIAL',
+         title: 'SPECIAL♥️',
          rowId: `${isPrefix}menutype 7`,
          description: ``
       }]
-      await client.sendList(m.chat, '', global.db.setting.msg, '© neoxr-bot v2.2.0', 'Tap!', [{
+      await client.sendList(m.chat, '', global.db.setting.msg, '© khurushi-bot v2.2.0', 'CLICK HERE!', [{
          rows
       }], m)
    },
